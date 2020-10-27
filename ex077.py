70d795659ca5a4302957b7fe978cb9a80e2291f6
Rodrigo Silva <rodrigonew199416@gmail.com>
2020-10-27 17:41:40 -0300

    Update ex077.py

--- ex077.py
+++ ex077.py
@@ -1,2 +1,9 @@
 ordens = ('Blatodeos', 'Coleopteros', 'Dipteros', 'Fasmideos', 'Ftirapteros', 'Hemipteros', 'Himenopteros',
           'Isopteros', 'Lepidopteros', 'Odonatos', 'Ortopteros', 'Tisanuros')
+for c in ordens:
+    print(f'\na palavra {c} tem as vogais ', end='') #cada palavra em 'ordens' também é uma lista (uma lista de letras)
+    for letra in c:
+        if letra.upper() in 'AEIOU':
+            print(letra, end=' ')
+
+#Não consegui fazer só. Preciso urgentemente treinar mais.
