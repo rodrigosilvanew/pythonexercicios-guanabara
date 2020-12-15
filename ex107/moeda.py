1dde99d82c7e2f1034acc3f39ac255b461179aae
Rodrigo Silva <rodrigonew199416@gmail.com>
2020-12-15 12:09:29 -0300

    ex107

---
+++ ex107/moeda.py
@@ -0,0 +1,15 @@
+def metade(n):
+    m = n / 2
+    return m
+
+def dobro(n):
+    m = n * 2
+    return m
+
+def aumento(n, t):
+    aum = (n * t/100) + n
+    return aum
+
+def reduz(n, t):
+    red = n - (n * t/100)
+    return red
